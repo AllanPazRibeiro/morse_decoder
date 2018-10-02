fee3c13cdc8277e46b2f79d5a2461e58a168957c
allan ribeiro <allan_paz_ribeiro@hotmail.com>
2018-10-02 01:19:25 -0300

    to querendo desistir dessa merda

--- morse_decoder.vhd
+++ morse_decoder.vhd
@@ -20,6 +20,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity morse_decoder is
     port (
@@ -31,78 +32,90 @@ entity morse_decoder is
 end morse_decoder;
 
 architecture Behavioral of morse_decoder is
-type GRUPOS is (GrupoUm, GrupoDois, GrupoTres, GrupoQuatro, NC, NP); -- Grupos em relacao aos tipos de letra
-signal next_state: GRUPOS; --Proximo estado recebe um do grupos
-signal current_state: std_logic_vector(7 downto 0); -- Variavel de contador
+--type GRUPOS is (GrupoUm, GrupoDois, GrupoTres, GrupoQuatro, NC, NP, Zero); -- Grupos em relacao aos tipos de letra
+--signal current_state, next_state: GRUPOS; --Proximo estado recebe um do grupos
+signal counter_up: integer;
+signal Um: std_logic;
+signal Dois: std_logic;
+
+signal current_state   : std_logic_vector(7 downto 0);
+signal next_state      : std_logic_vector(7 downto 0);
+
+signal NP            : std_logic;
+signal NC            : std_logic;
+signal GrupoUm       : std_logic;
+signal GrupoDois     : std_logic;
+signal GrupoTres     : std_logic;
+signal GrupoQuatro   : std_logic;
+
 
 --Recebe as entradas de dois em dois
 begin
 Counter_process: process(clock, reset)
 begin
     if(reset='1') then
-        current_state <= b"0"; 
+        current_state <= "000000000";
+        counter_up <= 0; 
     elsif (rising_edge(clock)) then
-        if(current_state<8) then
-            current_state <= current_state + "0000000";
-        end if;
- end if;
+       current_state <= next_state + "1"; 
+       counter_up <= counter_up + 1;
+ end if; 
 end process;
 
--- Definindo os processos
-case next_state is
-    when current_state = "10000000"                                    => NC        else 
-    when current_state = "01000000"                                    => NP        else
-    when current_state /= b"10000000" and current_state /= b"01000000" => GrupoUm   else
-    when current_state /= b"10000000" and current_state /= b"01000000" => GrupoDois else
-    when current_state /= b"10000000" and current_state /= b"01000000" => GrupoTres else
-    when current_state /= b"10000000" and current_state /= b"01000000" => GrupoQuatro;
-end case;
+NP          <= '1' when current_state = "000000010";
+NC          <= '1' when current_state = "000000001";
+GrupoUm     <= '1' when current_state = "000000000";
+GrupoDois   <= '1' when current_state = "000000000";
+GrupoTres   <= '1' when current_state = "000000000";
+GrupoQuatro <= '1' when current_state = "000000000"; 
 
--- Decodifica e exibe na tela as palavras/ letras
 Output_process: process(NC, current_state)
 begin
-    case current_state is
-        when NP  =>
+        if NP = '1' then
             saida <= x"20";
+        end if;
  -- GrupoUm -> E, T
-        when GrupoUm =>
-            if(entrada = b"000000000") then
+        if GrupoUm = '1' then
+            if(entrada = "000000000") then
                 saida <= x"45";
             elsif (entrada = b"11") then
                 saida <= x"54";
             end if;
+        end if;    
 -- GrupoDois -> A, I, M, N
-        when GrupoDois =>
-            if(entrada = b"0011") then
+        if GrupoDois = '1' then
+            if(entrada = b"00110000") then
                 saida <= x"41";
-            elsif (entrada = b"0000") then
+            elsif (entrada = b"00000000") then
                 saida <= x"49";
-            elsif (entrada = b"1111") then
+            elsif (entrada = b"11110000") then
                 saida <= x"4D";
-            elsif (entrada = b"1100") then
+            elsif (entrada = b"11000000") then
                 saida <= x"4E";
             end if;
+        end if;    
 -- GrupoTres -> D, G, K, O, R, S, U, W
-        when GrupoTres =>
-            if(entrada = b"110000") then
+        if GrupoTres = '1' then
+            if(entrada = b"11000000") then
                 saida <= x"44";
-            elsif (entrada = b"111100") then
+            elsif (entrada = b"11110000") then
                 saida <= x"47";
-            elsif (entrada = b"110011") then
+            elsif (entrada = b"11001100") then
                 saida <= x"4B";
-            elsif (entrada = b"111111") then
+            elsif (entrada = b"11111100") then
                 saida <= x"4F";
-            elsif (entrada = b"001100") then
+            elsif (entrada = b"00110000") then
                 saida <= x"52";
-            elsif (entrada = b"000000") then
+            elsif (entrada = b"00000000") then
                 saida <= x"53";
-            elsif (entrada = b"000011") then
+            elsif (entrada = b"00001100") then
                 saida <= x"55";
-            elsif (entrada = b"001111") then
+            elsif (entrada = b"00111100") then
                 saida <= x"57";
             end if;
+        end if;            
 -- GrupoQuatro -> B, C, F, H, J, L, P, Q, V, X, Y, Z
-    when GrupoQuatro '1' =>
+    if GrupoQuatro = '1' then
         if(entrada=b"11000000") then
             saida <= x"42";
         elsif (entrada=b"11001100") then
@@ -115,7 +128,7 @@ begin
             saida <= x"4A";
         elsif (entrada=b"00110000") then
             saida <= x"4C";
-        elsif (entrada=b"00111100") then
+        elsif entrada = "00111100" then
             saida <= x"50";
         elsif (entrada=b"11110011") then
             saida <= x"51";
@@ -128,6 +141,6 @@ begin
         elsif (entrada=b"11110000") then
             saida <= x"5A";
         end if;
-       end case;
+    end if;   
     end process;
 end Behavioral;
